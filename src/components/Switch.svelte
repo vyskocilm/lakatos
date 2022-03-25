eb24e068d50448ad3a097992cf60c1dfd5426991
Michal Vyskocil <michal.vyskocil@gmail.com>
2022-03-25 16:39:22 +0100

    Move switch to separate component
    Cleans the spaghetti mess of a business logic, which was happening
    there. This is a first step towards much cleaner code with a proper
    separation of concerns.
    
    Also remove vim temp file

---
+++ src/components/Switch.svelte
@@ -0,0 +1,53 @@
+<script>
+	import { createEventDispatcher } from 'svelte'
+
+	export let checked = false
+	export let disabled = false
+	export let text
+
+	let elSwitch
+
+	const dispatch = createEventDispatcher()
+	function toggle() {
+		checked = !checked
+		dispatch('click', {
+			checked: checked,
+		})
+	}
+
+	const style = {
+		switch: `toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full`,
+	}
+
+	$: {
+		if (elSwitch != undefined) {
+			elSwitch.style.cursor = disabled ? 'not-allowed' : ''
+		}
+	}
+</script>
+
+<label
+	for="nsfw-switch1"
+	class="flex items-center cursor-pointer relative mb-4"
+	bind:this={elSwitch}
+>
+	<input type="checkbox" id="nsfw-switch1" class="sr-only" {checked} {disabled} on:click={toggle} />
+	<div class={style.switch} />
+	<span class="ml-3 text-gray-900 text-sm font-medium">{text}</span>
+</label>
+
+<style>
+	.toggle-bg:after {
+		content: '';
+		@apply absolute top-0.5 left-0.5 bg-white border border-gray-300 rounded-full h-5 w-5 transition shadow-sm;
+	}
+
+	input:checked + .toggle-bg:after {
+		transform: translateX(100%);
+		@apply border-white;
+	}
+
+	input:checked + .toggle-bg {
+		@apply bg-blue-600 border-blue-600;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,10 @@
 <script>
+	import Switch from '../components/Switch.svelte'
+
 	const style = {
 		h1: `text-5xl text-center my-8 uppercase`,
 		grid: `py-4 grid gap-4 md:grid-cols-3 grid-cols-1`,
 		btn: `p-8 shrink max-w-sm text-xl text-white font-bold flex flex-col items-center text-center bg-blue-500 rounded hover:bg-blue-700 font-bold disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-blue-500`,
-		switchEnabled: `toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full`,
-		switchDisabled: `toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full`,
 	}
 
 	const lakatosAll = [
@@ -30,8 +30,6 @@
 	// DOM objects
 	// slice of audio objects
 	let audios = []
-	// nsfwSwitchRound
-	let nsfwSwitchRound
 	// other variables
 	// control whenever play buttons shall be enabled/disabled
 	let soundEnabled = true
@@ -40,27 +38,20 @@
 
 	async function play({ idx }) {
 		soundEnabled = false
-		// FIXME: this mixes a logic of two components
-		nsfwSwitchRound.style.cursor = 'not-allowed'
 		await audios[idx].play()
 	}
 </script>
 
 <h1 class={style.h1}>Svelte Kit Lakatos</h1>
 
-<label for="nsfw-switch" class="flex items-center cursor-pointer relative mb-4">
-	<input
-		type="checkbox"
-		id="nsfw-switch"
-		class="sr-only"
-		disabled={!soundEnabled}
-		on:click={() => {
-			nsfwEnabled = !nsfwEnabled
-		}}
-	/>
-	<div id="foo" class={style.switchEnabled} bind:this={nsfwSwitchRound} />
-	<span class="ml-3 text-gray-900 text-sm font-medium">NSFW</span>
-</label>
+<!--TODO: initial checked= works in a weird way -->
+<Switch
+	disabled={!soundEnabled}
+	on:click={(e) => {
+		nsfwEnabled = e.detail.checked
+	}}
+	text="NSFW"
+/>
 
 <!--TODO: REALLY REALLY REALLY - use components and a store here!!!-->
 <!-- initialize ALL audios before -->
@@ -71,8 +62,6 @@
 		bind:this={audios[idx]}
 		on:ended={() => {
 			soundEnabled = true
-			// FIXME: this mixes a logic of two components
-			nsfwSwitchRound.style.cursor = ''
 		}}
 	>
 		<source src={laka.src} type="audio/mpeg" />
@@ -96,19 +85,3 @@
 		{/if}
 	{/each}
 </div>
-
-<style>
-	.toggle-bg:after {
-		content: '';
-		@apply absolute top-0.5 left-0.5 bg-white border border-gray-300 rounded-full h-5 w-5 transition shadow-sm;
-	}
-
-	input:checked + .toggle-bg:after {
-		transform: translateX(100%);
-		@apply border-white;
-	}
-
-	input:checked + .toggle-bg {
-		@apply bg-blue-600 border-blue-600;
-	}
-</style>
