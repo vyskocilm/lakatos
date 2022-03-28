92329d9e98737edc276a7cec8bf137866e3f8fc8
Michal Vyskocil <michal.vyskocil@gmail.com>
2022-03-28 17:36:17 +0200

    move buttons+audio to SoundClips component

---
+++ src/components/SoundClips.svelte
@@ -0,0 +1,50 @@
+<script>
+	import { createEventDispatcher } from 'svelte'
+	export let clips
+	export let nsfwEnabled
+	export let soundEnabled
+
+	const dispatch = createEventDispatcher()
+
+	let audios = []
+
+	async function play({ idx }) {
+		soundEnabled = false
+		dispatch('play', { soundEnabled: soundEnabled })
+		await audios[idx].play()
+	}
+
+	const style = {
+		grid: `py-4 grid gap-4 md:grid-cols-3 grid-cols-1`,
+		btn: `p-8 shrink max-w-sm text-xl text-white font-bold flex flex-col items-center text-center bg-blue-500 rounded hover:bg-blue-700 font-bold disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-blue-400`,
+	}
+</script>
+
+{#each clips as clip, idx}
+	<audio
+		preload="auto"
+		id="`audio-{idx}`"
+		bind:this={audios[idx]}
+		on:ended={() => {
+			soundEnabled = true
+			dispatch('ended', { soundEnabled: soundEnabled })
+		}}
+	>
+		<source src={clip.src} type="audio/mpeg" />
+	</audio>
+{/each}
+
+<div class={style.grid}>
+	{#each clips as clip, idx}
+		{#if nsfwEnabled && !clip.sfw}
+			<button class={style.btn} on:click={() => play({ idx })} disabled={!soundEnabled}
+				>{clip.title}</button
+			>
+		{/if}
+		{#if clip.sfw}
+			<button class={style.btn} on:click={() => play({ idx })} disabled={!soundEnabled}
+				>{clip.title}</button
+			>
+		{/if}
+	{/each}
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,12 @@
 <script>
 	import Switch from '../components/Switch.svelte'
+	import SoundClips from '../components/SoundClips.svelte'
 
 	const style = {
 		h1: `text-5xl text-center my-8 uppercase`,
-		grid: `py-4 grid gap-4 md:grid-cols-3 grid-cols-1`,
-		btn: `p-8 shrink max-w-sm text-xl text-white font-bold flex flex-col items-center text-center bg-blue-500 rounded hover:bg-blue-700 font-bold disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-blue-500`,
 	}
 
+	// move to store????
 	const lakatosAll = [
 		{
 			title: 'Ani za kokot vole!',
@@ -21,25 +21,17 @@
 			src: 'http://milujipraci.cz/sfx/hajzli-jedni.mp3',
 		},
 		{
-			title: 'Toto je neuveritelne!',
 			sfw: true,
+			title: 'Toto je neuveritelne!',
 			src: 'http://milujipraci.cz/sfx/hosi-to-je-neuveritelne.mp3',
 		},
 	]
 
-	// DOM objects
-	// slice of audio objects
-	let audios = []
-	// other variables
-	// control whenever play buttons shall be enabled/disabled
+	// internal state
+	// soundEnabled control whenever play buttons shall be enabled/disabled
 	let soundEnabled = true
-	// if nsfw (with cursing) shall be enabled
+	// nsfwEnabled if (with cursing) shall be enabled
 	let nsfwEnabled = false
-
-	async function play({ idx }) {
-		soundEnabled = false
-		await audios[idx].play()
-	}
 </script>
 
 <h1 class={style.h1}>Svelte Kit Lakatos</h1>
@@ -53,35 +45,14 @@
 	text="NSFW"
 />
 
-<!--TODO: REALLY REALLY REALLY - use components and a store here!!!-->
-<!-- initialize ALL audios before -->
-{#each lakatosAll as laka, idx}
-	<audio
-		preload="auto"
-		id={idx}
-		bind:this={audios[idx]}
-		on:ended={() => {
-			soundEnabled = true
-		}}
-	>
-		<source src={laka.src} type="audio/mpeg" />
-		<embed hidden="true" autostart="false" loop="false" src={laka.src} />
-	</audio>
-{/each}
-
-<div class={style.grid}>
-	{#each lakatosAll as laka, idx}
-		{#if nsfwEnabled && !laka.sfw}
-			<!--TODO: move to component -->
-			<button class={style.btn} on:click={() => play({ idx })} disabled={!soundEnabled}
-				>{laka.title}</button
-			>
-		{/if}
-		{#if laka.sfw}
-			<!--TODO: move to component -->
-			<button class={style.btn} on:click={() => play({ idx })} disabled={!soundEnabled}
-				>{laka.title}</button
-			>
-		{/if}
-	{/each}
-</div>
+<SoundClips
+	clips={lakatosAll}
+	{soundEnabled}
+	{nsfwEnabled}
+	on:ended={() => {
+		soundEnabled = true
+	}}
+	on:play={() => {
+		soundEnabled = false
+	}}
+/>
