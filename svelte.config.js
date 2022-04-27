d6b888ba6342767edcf7ed04272e4dd82bb1c77f
Michal Vyskocil <michal.vyskocil@gmail.com>
2022-04-27 20:43:16 +0200

    revert back to auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,14 +10,7 @@ const config = {
 		assets: 'static',
 	 },
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
-
+		adapter: adapter(),
 		prerender: {
 			// This can be false if you're using a fallback (i.e. SPA mode)
 			default: true
